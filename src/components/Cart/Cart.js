fbaee23d845cff0eec6032bb7c8e117deb2ab626
ZNP8b <znp8b00ff@gmail.com>
2022-10-07 02:30:56 +0300

    React cart_card component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import styles from "./Cart.module.scss";
+import Cart_Card from "./Cart_Card/Cart_Card";
 
 export function Cart() {
     return (
@@ -8,28 +9,7 @@ export function Cart() {
             <Header />
             <div className="content">
                 <h2>Корзина</h2>
-
-                <div className={styles.cart_card}>
-                    <div className={styles.leftSide}>
-                        <img width={146} height={136} src="img/1.jpg" />
-                        <div className={styles.quantityIcons}>
-                            <img width={36} height={30} src="img/minus.svg" />
-                            <p className={styles.quantity}>1</p>
-                            <img width={36} height={30} src="img/plus.svg" />
-                        </div>
-                    </div>
-
-                    <div className={styles.middle}>
-                        <p className={styles.name}>Apple BYZ S852I</p>
-                        <p className={styles.price}>2 927 ₽</p>
-                    </div>
-
-                    <div className={styles.rightSide}>
-                        <img width={21} height={17} src="img/delete.svg" />
-                        <p className={styles.fullPrice}>2 927 ₽</p>
-                    </div>
-                </div>
-
+                <Cart_Card />
             </div>
             <Footer />
         </div>
