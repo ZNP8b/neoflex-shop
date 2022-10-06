6ca8179a21bc4c5573e3177b7411c0686cef445a
ZNP8b <znp8b00ff@gmail.com>
2022-10-07 00:10:54 +0300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
-import Card from "./components/Card/Card";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-
-const data_Wired = [
-  { img: "img/1.jpg", name: "Apple BYZ S852I", price: "2927", rating: "4.7" },
-  { img: "img/2.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" },
-  { img: "img/3.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" },
-  { img: "img/1.jpg", name: "Apple BYZ S852I", price: "2927", rating: "4.7" },
-  { img: "img/2.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" },
-  { img: "img/3.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" }
-];
-
-const data_Wireless = [
-  { img: "img/4.jpg", name: "Apple AirPods", price: "9527", rating: "4.7" },
-  { img: "img/5.jpg", name: "GERLAX GH-04", price: "6527", rating: "4.7" },
-  { img: "img/6.jpg", name: "BOROFONE BO4", price: "7527", rating: "4.7" },
-];
+import { Shop } from "./components/Shop/Shop";
+import { Routes, Route, Link } from "react-router-dom";
+import { Cart } from "./components/Cart/Cart";
 
 function App() {
   return (
     <div className="wrapper">
-      <div className="content" >
-        <Header />
-        <h1>Наушники</h1>
-        <div className="cards">
-
-          {data_Wired.map((val) => (
-            <Card img={val.img} name={val.name} price={val.price} rating={val.rating} />
-          )) }
-
-        </div>
-        <h1>Беспроводные наушники</h1>
-        <div className="cards">
-          
-            {data_Wireless.map((val) => (
-              <Card img={val.img} name={val.name} price={val.price} rating={val.rating} />
-            ))}
-
-        </div>
-      </div>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
