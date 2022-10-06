76147350f041757ecc8c814295635f6662ab12c6
ZNP8b <znp8b00ff@gmail.com>
2022-10-06 22:28:32 +0300

    Added arrays with data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 
+const data_Wired = [
+  { img: "img/1.jpg", name: "Apple BYZ S852I", price: "2927", rating: "4.7" },
+  { img: "img/2.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" },
+  { img: "img/3.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" },
+  { img: "img/1.jpg", name: "Apple BYZ S852I", price: "2927", rating: "4.7" },
+  { img: "img/2.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" },
+  { img: "img/3.jpg", name: "Apple EarPods", price: "2327", rating: "4.5" }
+];
+
+const data_Wireless = [
+  { img: "img/4.jpg", name: "Apple AirPods", price: "9527", rating: "4.7" },
+  { img: "img/5.jpg", name: "GERLAX GH-04", price: "6527", rating: "4.7" },
+  { img: "img/6.jpg", name: "BOROFONE BO4", price: "7527", rating: "4.7" },
+];
+
 function App() {
   return (
     <div className="wrapper">
@@ -9,18 +24,19 @@ function App() {
         <Header />
         <h1>Наушники</h1>
         <div className="cards">
-          <Card img="img/1.jpg" name="Apple BYZ S852I" price="2927" rating="4.7" />
-          <Card img="img/2.jpg" name="Apple EarPods" price="2327" rating="4.5" />
-          <Card img="img/3.jpg" name="Apple EarPods" price="2327" rating="4.5" />
-          <Card img="img/1.jpg" name="Apple BYZ S852I" price="2927" rating="4.7" />
-          <Card img="img/2.jpg" name="Apple EarPods" price="2327" rating="4.5" />
-          <Card img="img/3.jpg" name="Apple EarPods" price="2327" rating="4.5" />
+
+          {data_Wired.map((val) => (
+            <Card img={val.img} name={val.name} price={val.price} rating={val.rating} />
+          )) }
+
         </div>
         <h1>Беспроводные наушники</h1>
         <div className="cards">
-          <Card img="img/4.jpg" name="Apple AirPods" price="9527" rating="4.7" />
-          <Card img="img/5.jpg" name="GERLAX GH-04" price="6527" rating="4.7" />
-          <Card img="img/6.jpg" name="BOROFONE BO4" price="7527" rating="4.7" />
+          
+            {data_Wireless.map((val) => (
+              <Card img={val.img} name={val.name} price={val.price} rating={val.rating} />
+            ))}
+
         </div>
       </div>
       <Footer />
