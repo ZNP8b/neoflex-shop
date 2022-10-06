b8996c9c4ff80353c2745c762449e956d3522478
ZNP8b <znp8b00ff@gmail.com>
2022-10-07 00:11:14 +0300

    new component Cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+
+export function Cart() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    );
+};
