c8f01ed81952d596600c95595e84a7d581786912
ZNP8b <znp8b00ff@gmail.com>
2022-10-06 19:56:11 +0300

    Card react component

--- src/App.js
+++ src/App.js
@@ -1,204 +1,39 @@
+import Card from "./components/Card/Card";
+
 
 function App() {
   return (
     <div className="wrapper">
-      <header>
-        <div className="headerLeft">
-          <p>QPICK</p>
-        </div>
-        <ul className="headerRight">
-          <li>
-            <img width={22} height={20} src="img/fav.svg" />
-          </li>
-          <li>
-            <img width={22} height={20} src="img/cart.svg" />
-          </li>
-        </ul>
-      </header>
-
       <div className="content" >
+      <header>
+            <div className="headerLeft">
+                <p>QPICK</p>
+            </div>
+            <ul className="headerRight">
+                <li>
+                    <img width={22} height={20} src="img/fav.svg" />
+                </li>
+                <li>
+                    <img width={22} height={20} src="img/cart.svg" />
+                </li>
+            </ul>
+        </header>
         <h1>Наушники</h1>
-
         <div className="cards">
-          <div className="card" >
-            <div className="product" >
-              <img src="img/1.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">Apple BYZ S852I</p>
-                <p className="price">2927 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.7</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/2.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">Apple EarPods</p>
-                <p className="price">2327 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.5</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/3.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">Apple EarPods</p>
-                <p className="price">2327 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.5</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/1.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">Apple BYZ S852I</p>
-                <p className="price">2927 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.7</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/2.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">Apple EarPods</p>
-                <p className="price">2327 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.5</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/3.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">Apple EarPods</p>
-                <p className="price">2327 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.5</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
+          <Card img="img/1.jpg" name="Apple BYZ S852I" price="2927" rating="4.7" />
+          <Card img="img/2.jpg" name="Apple EarPods" price="2327" rating="4.5" />
+          <Card img="img/3.jpg" name="Apple EarPods" price="2327" rating="4.5" />
+          <Card img="img/1.jpg" name="Apple BYZ S852I" price="2927" rating="4.7" />
+          <Card img="img/2.jpg" name="Apple EarPods" price="2327" rating="4.5" />
+          <Card img="img/3.jpg" name="Apple EarPods" price="2327" rating="4.5" />
         </div>
-
         <h1>Беспроводные наушники</h1>
         <div className="cards">
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/4.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">Apple AirPods</p>
-                <p className="price">9527 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.7</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/5.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">GERLAX GH-04</p>
-                <p className="price">6527 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.7</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="card" >
-            <div className="product" >
-              <img src="img/6.jpg" className="product_picture" />
-            </div>
-            <div className="card_content">
-              <div className="card_content_title">
-                <p className="name">BOROFONE BO4</p>
-                <p className="price">7527 ₽</p>
-              </div>
-              <div className="card_content_purchase">
-                <div className="card_content_rating">
-                  <img src="img/star.svg" />
-                  <p>4.7</p>
-                </div>
-                <a href="#">Купить</a>
-              </div>
-            </div>
-          </div>
-
+        <Card img="img/4.jpg" name="Apple AirPods" price="9527" rating="4.7" />
+        <Card img="img/5.jpg" name="GERLAX GH-04" price="6527" rating="4.7" />
+        <Card img="img/6.jpg" name="BOROFONE BO4" price="7527" rating="4.7" />
         </div>
       </div>
-
       <footer>
 
         <div className="footerLogo" >
@@ -223,11 +58,11 @@ function App() {
               <a href="#">Условия сервиса</a>
             </div>
             <div className="footerLang_icon">
-              <img className="footerLang_icon_1st" src="img/lang.svg" />
-              <div className="footerLang_icon_2nd">
+              <img src="img/lang.svg" />
+              <div className="footerLang_icon_mid">
                 <a href="#">Рус</a>
               </div>
-              <div className="footerLang_icon_3rd">
+              <div>
                 <a href="#">Eng</a>
               </div>
             </div>
