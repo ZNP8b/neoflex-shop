c191ba5dae1dcb038e32f389ce6040cab4203653
ZNP8b <znp8b00ff@gmail.com>
2022-10-07 16:38:36 +0300

    added array of data for Cart Card

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,10 @@ import styles from "./Cart.module.scss";
 import Cart_Card from "./Cart_Card/Cart_Card";
 import Result_Card from "./Result_Card/Result_Card";
 
+const cart_data = [
+    {img: "img/1.jpg", name:"Apple BYZ S852I", price: "2 927", fullPrice: "2 927"},
+]
+
 export function Cart() {
     return (
         <div>
@@ -12,7 +16,11 @@ export function Cart() {
                 <h2>Корзина</h2>
                 <div className={styles.cart_content}>
                     <div className={styles.cart_cards}>
-                        <Cart_Card img="img/1.jpg" name="Apple BYZ S852I" price="2 927" fullPrice="2 927" />
+                        {
+                            cart_data.map((val) => (
+                                <Cart_Card img={val.img} name={val.name} price={val.price} fullPrice={val.fullPrice} />
+                            ))
+                        }
                     </div>
                     <Result_Card fullPrice="2 927" />
                 </div>
