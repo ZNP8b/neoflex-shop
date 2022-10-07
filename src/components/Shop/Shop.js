75460e91805726117fdcae90da4736a770483b55
ZNP8b <znp8b00ff@gmail.com>
2022-10-07 19:57:05 +0300

    added useState for quantity

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,17 +1,20 @@
+import React from "react";
 import Header from "../Header/Header"
 import Card from "../Card/Card"
 import Footer from "../Footer/Footer"
 
 export function Shop(props) {
+const [quantity, setQuantity] = React.useState(0);
+
     return (
         <div>
-            <Header />
+            <Header quantity={quantity} />
             <div className="content" >
                 <h1>Наушники</h1>
                 <div className="cards">
 
                     {props.shopData.data_Wired.map((val) => (
-                        <Card img={val.img} name={val.name} price={val.price} rating={val.rating} />
+                        <Card onClickBuy={() => setQuantity(quantity + 1)} img={val.img} name={val.name} price={val.price} rating={val.rating} />
                     ))}
 
                 </div>
@@ -19,7 +22,7 @@ export function Shop(props) {
                 <div className="cards">
 
                     {props.shopData.data_Wireless.map((val) => (
-                        <Card img={val.img} name={val.name} price={val.price} rating={val.rating} />
+                        <Card onClickBuy={() => setQuantity(quantity + 1)} img={val.img} name={val.name} price={val.price} rating={val.rating} />
                     ))}
 
                 </div>
