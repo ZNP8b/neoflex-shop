1b33a92ca5426461518f82cf2173ac29a11aeeae
ZNP8b <znp8b00ff@gmail.com>
2022-10-07 16:00:52 +0300

    added props to cart card

--- src/components/Cart/Cart_Card/Cart_Card.js
+++ src/components/Cart/Cart_Card/Cart_Card.js
@@ -1,10 +1,10 @@
 import styles from "./Cart_Card.module.scss";
 
-function Cart_Card() {
+function Cart_Card(props) {
     return (
         <div className={styles.cart_card}>
             <div className={styles.leftSide}>
-                <img width={146} height={136} src="img/1.jpg" />
+                <img width={146} height={136} src={props.img} />
                 <div className={styles.quantityIcons}>
                     <img width={36} height={30} src="img/minus.svg" />
                     <p className={styles.quantity}>1</p>
@@ -13,13 +13,13 @@ function Cart_Card() {
             </div>
 
             <div className={styles.middle}>
-                <p className={styles.name}>Apple BYZ S852I</p>
-                <p className={styles.price}>2 927 ₽</p>
+                <p className={styles.name}>{props.name}</p>
+                <p className={styles.price}>{props.price} ₽</p>
             </div>
 
             <div className={styles.rightSide}>
                 <img width={21} height={17} src="img/delete.svg" />
-                <p className={styles.fullPrice}>2 927 ₽</p>
+                <p className={styles.fullPrice}>{props.fullPrice} ₽</p>
             </div>
         </div>
     );
