9504a9f5907f4daa1d00a50c4083ee245f296085
ZNP8b <znp8b00ff@gmail.com>
2022-10-08 01:10:14 +0300

    fullPrice fixes

--- src/components/Cart/Cart_Card/Cart_Card.js
+++ src/components/Cart/Cart_Card/Cart_Card.js
@@ -1,13 +1,14 @@
 import styles from "./Cart_Card.module.scss";
 
 function Cart_Card(props) {
+    const quantity = 1;
     return (
         <div className={styles.cart_card}>
             <div className={styles.leftSide}>
                 <img width={146} height={136} src={props.img} />
                 <div className={styles.quantityIcons}>
                     <img width={36} height={30} src="img/minus.svg" />
-                    <p className={styles.quantity}>1</p>
+                    <p className={styles.quantity}>{quantity}</p>
                     <img width={36} height={30} src="img/plus.svg" />
                 </div>
             </div>
@@ -19,7 +20,7 @@ function Cart_Card(props) {
 
             <div className={styles.rightSide}>
                 <img width={21} height={17} src="img/delete.svg" />
-                <p className={styles.fullPrice}>{props.fullPrice} ₽</p>
+                <p className={styles.fullPrice}>{props.price * quantity} ₽</p>
             </div>
         </div>
     );
