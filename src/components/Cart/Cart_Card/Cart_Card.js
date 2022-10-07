ad9269ff42c840d040472658d32194dd7c0de9f7
ZNP8b <znp8b00ff@gmail.com>
2022-10-08 01:43:34 +0300

    changed props to 1 item

--- src/components/Cart/Cart_Card/Cart_Card.js
+++ src/components/Cart/Cart_Card/Cart_Card.js
@@ -1,26 +1,25 @@
 import styles from "./Cart_Card.module.scss";
 
 function Cart_Card(props) {
-    const quantity = 1;
     return (
         <div className={styles.cart_card}>
             <div className={styles.leftSide}>
-                <img width={146} height={136} src={props.img} />
+                <img width={146} height={136} src={props.item.img} />
                 <div className={styles.quantityIcons}>
                     <img width={36} height={30} src="img/minus.svg" />
-                    <p className={styles.quantity}>{quantity}</p>
+                    <p className={styles.quantity}>{props.item.quantity}</p>
                     <img width={36} height={30} src="img/plus.svg" />
                 </div>
             </div>
 
             <div className={styles.middle}>
-                <p className={styles.name}>{props.name}</p>
-                <p className={styles.price}>{props.price} ₽</p>
+                <p className={styles.name}>{props.item.name}</p>
+                <p className={styles.price}>{props.item.price} ₽</p>
             </div>
 
             <div className={styles.rightSide}>
                 <img width={21} height={17} src="img/delete.svg" />
-                <p className={styles.fullPrice}>{props.price * quantity} ₽</p>
+                <p className={styles.fullPrice}>{props.item.price * props.item.quantity} ₽</p>
             </div>
         </div>
     );
