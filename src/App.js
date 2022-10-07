ab7b078bc7b16e375b0a96e1116469fee6528a9b
ZNP8b <znp8b00ff@gmail.com>
2022-10-08 01:08:24 +0300

    added useState to change quantity on cart icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
 import { Shop } from "./components/Shop/Shop";
 import { Routes, Route, Link } from "react-router-dom";
 import { Cart } from "./components/Cart/Cart";
 
 function App(props) {
+  props.data.cartPage.cart_data = JSON.parse(sessionStorage.getItem("order_items")) || [];
+  const [quantity, setQuantity] = React.useState(props.data.cartPage.cart_data.length);
+
   const addToOrder = (item) => {
     props.data.cartPage.cart_data = JSON.parse(sessionStorage.getItem("order_items")) || [];
     props.data.cartPage.cart_data.push(item);
@@ -12,8 +16,8 @@ function App(props) {
   return (
     <div className="wrapper">
       <Routes>
-        <Route path="/" element={<Shop onAdd={addToOrder} shopData={props.data.shopPage} />} />
-        <Route path="/cart" element={<Cart cartData={props.data.cartPage} />} />
+        <Route path="/" element={<Shop quantity={quantity} onClickBuy={() => setQuantity(quantity + 1)} onAdd={addToOrder} shopData={props.data.shopPage} />} />
+        <Route path="/cart" element={<Cart quantity={quantity} cartData={props.data.cartPage} />} />
       </Routes>
     </div>
   );
