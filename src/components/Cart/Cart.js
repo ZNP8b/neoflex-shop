7ff053947de6f94a4756368391f4ef8a8fd639c5
ZNP8b <znp8b00ff@gmail.com>
2022-10-07 16:00:17 +0300

    result card

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,7 +9,18 @@ export function Cart() {
             <Header />
             <div className="content">
                 <h2>Корзина</h2>
-                <Cart_Card />
+                <div className={styles.cart_content}>
+                    <div className={styles.cart_cards}>
+                    <Cart_Card img="img/1.jpg" name="Apple BYZ S852I" price="2 927" fullPrice="2 927" />
+                    </div>
+                    <div className={styles.result_card}>
+                        <div className={styles.finalPrice}>
+                            <p>ИТОГО</p>
+                            <p >₽ 2 927</p>
+                        </div>
+                        <button>Перейти к оформлению</button>
+                    </div>
+                </div>
             </div>
             <Footer />
         </div>
