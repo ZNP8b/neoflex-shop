b5cc98e1e1a4af55936b027a184f62b421d15b29
ZNP8b <znp8b00ff@gmail.com>
2022-10-08 17:27:50 +0300

    moved array of data to another file

---
+++ src/state.js
@@ -0,0 +1,26 @@
+let state = {
+    shopPage: {
+        data_Wired: [
+            { img: "img/1.jpg", name: "Apple BYZ S852I", price: "2927", rating: "4.7", quantity: "1" },
+            { img: "img/2.jpg", name: "Apple EarPods", price: "2327", rating: "4.5", quantity: "1" },
+            { img: "img/3.jpg", name: "Apple EarPods", price: "2327", rating: "4.5", quantity: "1" },
+            { img: "img/1.jpg", name: "Apple BYZ S852I", price: "2927", rating: "4.7", quantity: "1" },
+            { img: "img/2.jpg", name: "Apple EarPods", price: "2327", rating: "4.5", quantity: "1" },
+            { img: "img/3.jpg", name: "Apple EarPods", price: "2327", rating: "4.5", quantity: "1" }
+        ],
+
+        data_Wireless: [
+            { img: "img/4.jpg", name: "Apple AirPods", price: "9527", rating: "4.7", quantity: "1" },
+            { img: "img/5.jpg", name: "GERLAX GH-04", price: "6527", rating: "4.7", quantity: "1" },
+            { img: "img/6.jpg", name: "BOROFONE BO4", price: "7527", rating: "4.7", quantity: "1" },
+        ]
+    },
+
+    cartPage: {
+        cart_data: [
+
+        ]
+    }
+}
+
+export default state;
