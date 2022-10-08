8eb52f71f078b9011035b5dd8106dac2adace04e
ZNP8b <znp8b00ff@gmail.com>
2022-10-08 03:23:02 +0300

    deleteFromOrder button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import { Cart } from "./components/Cart/Cart";
 
 function App(props) {
+
   props.data.cartPage.cart_data = JSON.parse(sessionStorage.getItem("order_items")) || [];
   const [quantity, setQuantity] = React.useState(props.data.cartPage.cart_data.length);
 
@@ -13,14 +14,19 @@ function App(props) {
     sessionStorage.setItem("order_items", JSON.stringify(props.data.cartPage.cart_data));
   }
 
-  return (
-    <div className="wrapper">
-      <Routes>
-        <Route path="/" element={<Shop quantity={quantity} onClickBuy={() => setQuantity(quantity + 1)} onAdd={addToOrder} shopData={props.data.shopPage} />} />
-        <Route path="/cart" element={<Cart quantity={quantity} cartData={props.data.cartPage} />} />
-      </Routes>
-    </div>
-  );
+  const deleteFromOrder = (id) => {
+    props.data.cartPage.cart_data.splice(id, 1);
+    sessionStorage.setItem("order_items", JSON.stringify(props.data.cartPage.cart_data));
+  }
+
+    return (
+      <div className="wrapper">
+        <Routes>
+          <Route path="/" element={<Shop quantity={quantity} onClickBuy={() => setQuantity(quantity + 1)} onAdd={addToOrder} shopData={props.data.shopPage} />} />
+          <Route path="/cart" element={<Cart deleteFromOrder={deleteFromOrder} quantity={quantity} onClickDelete={() => setQuantity(quantity - 1)} cartData={props.data.cartPage} />} />
+        </Routes>
+      </div>
+    );
 }
 
 export default App;
